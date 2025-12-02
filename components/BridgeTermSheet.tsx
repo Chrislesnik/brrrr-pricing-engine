ac1f125fbf7a88e513c73698dd1f74fb3c761b1a
Chris Lesnik <cglesnik@gmail.com>
2025-12-02 16:45:14 -0500

    Term Sheet modal: center sheet vertically and horizontally using min-h-svh + items-center justify-center

--- components/BridgeTermSheet.tsx
+++ components/BridgeTermSheet.tsx
@@ -112,9 +112,9 @@ const BridgeTermSheet = (props: BridgeTermSheetProps): React.ReactElement => {
   }
 
   return (
-    <div className="overflow-x-hidden w-full min-w-[816px] min-h-[1056px] h-full flex items-center justify-center bg-white text-black">
+    <div className="overflow-x-hidden w-full min-w-[816px] min-h-svh flex items-center justify-center bg-white text-black">
       {/* Reduce page padding to 'zoom out' slightly */}
-      <div className="w-[816px] max-w-none p-3 relative print:p-2">
+      <div className="w-[816px] max-w-none mx-auto p-3 relative print:p-2">
         {/* Option number bracketed by solid black rules */}
         <div className="mb-3">
           <div className="h-[2px] bg-black" />
