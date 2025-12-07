d52c4df831e0e812c6fc6037a55b8d7be51c8a5a
Chris Lesnik <cglesnik@gmail.com>
2025-12-06 19:00:21 -0500

    Remove extra row 12; keep extra rows 1-11 above EMD in DEBITS

--- components/DSCRTermSheet.tsx
+++ components/DSCRTermSheet.tsx
@@ -204,8 +204,8 @@ const DscrSheet = (props: DSCRTermSheetProps) => {
                     </div>
                   ))}
                 </div>
-                {/* Extra rows 1-12 above EMD */}
-                {[1,2,3,4,5,6,7,8,9,10,11,12].map((n) => (
+                {/* Extra rows 1-11 above EMD */}
+                {[1,2,3,4,5,6,7,8,9,10,11].map((n) => (
                   <div key={`extra-${n}`} className="flex items-center justify-between text-xs mt-1">
                     <span className="pl-2">{asText(props, `extra_row_label_${n}`)}</span>
                     <span>{asText(props, `row_label_${n}`)}</span>
